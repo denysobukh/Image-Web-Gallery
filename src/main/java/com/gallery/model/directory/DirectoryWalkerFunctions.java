37469ced241a3beb22adde84e821db748f55c869
Denis Obukhov <denys.obukhov@gmail.com>
2019-03-28 14:57:30 +0200

    DirectoryWalker was made immutable

---
+++ src/main/java/com/gallery/model/directory/DirectoryWalkerFunctions.java
@@ -0,0 +1,11 @@
+package com.gallery.model.directory;
+
+/**
+ * Project: Gallery
+ * Class: DirectoryWalkerFunctions
+ * Date: 2019-03-26 20:11 [Tuesday]
+ *
+ * @author Dennis Obukhov
+ */
+public interface DirectoryWalkerFunctions {
+}
